48998f9fd9bb1a290df5d82490619d00dda874f0
msvelasquez <msantiago.velasquez@udea.edu.co>
2021-12-15 22:56:42 -0500

    Desarolados ejercicios 6, 10, 12, 14.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
@@ -12,6 +13,13 @@ using namespace std;
 
 */
 
+//Funciones utilizadas en los ejercicios (definidas al final del archivo)
+
+bool esPrimo(unsigned int a);
+int factorial(unsigned short int a);
+bool esPali(unsigned int a);
+int digitos(unsigned int a);
+int flipNum(unsigned int a);
 
 /*
 
@@ -91,10 +99,12 @@ int main(){
     cout << "100: " << cien << endl;
     cout << "50: " << cinq10 << endl;
     cout << "Faltante: " << moni << endl;
+
+    return 0;
 }
 */
 
-//
+/*
 
 //Problema #4 Tiempo transcurrido en hora militar
 
@@ -112,7 +122,7 @@ int main(){
      cout << "Ingrese hora inicial: "; //Ingreso hora inicial
      cin >> hora0;
      while( hora0/100>24 || hora0%100>59 ){ //Comprobador de hora valida
-         cout << hora0 << " es un tiempo invalido, ingrese tiempo transcurrido: ";
+         cout << hora0 << " es una hora invalida, ingrese hora inicial: ";
          cin >> hora0;
      }
 
@@ -141,70 +151,190 @@ int main(){
 
 
      //Salida de la hora resultante
-    cout << "La hora es " << hora2 << ".";
+    cout << "La hora es " << hora2 << "." << endl;
 
+
+    return 0;
+ }
+*/
+
+/*
+
+   //Problema #6 Valor aproximade de euler
+
+int main(){
+    //Inicializacion de variables
+    unsigned int elementos = 0; //Variable para cantidad de elementos a aproximar
+    float resultado = 0; //Variable de resultado aproximado de euler
+
+    //Ingresos del usuario
+    cout << "Ingrese elementos usados para la aproximacion: ";
+    cin >> elementos;
+
+    //Calculo de euler
+    for(unsigned short int a = 0; a < elementos; a++){
+        resultado = resultado + (float(1)/float(factorial(a)));
+    }
+
+
+    //Salidas
+    cout << "e es aproximadamente: " << resultado << endl;
+
+    return 0;
+ }
+*/
+
+//
+
+   //Problema #8 Imprimir suma de multiplos unicos
+
+int main(){
+    unsigned int a = 0; //Primer numero para generar multiplos
+    unsigned int b = 0; //Segundo numero para generar multiplos
+    unsigned int c = 0; //Maximo valor de los multiplos
+
+
+return 0;
  }
 //
 
 /*
 
-   Problema #6
+   //Problema #10 enesimo numero primo
 
 int main(){
 
+    //Inicializacion de variables
+    unsigned int n = 981; //Posicion del numero primo
+    unsigned int primo = 1; //Numero primo en la posicion n
+
+    while(n > 0){
+        primo++;
+        if(esPrimo(primo)){
+            n -= 1;
+        }
+
+    }
+    //Salidas
+    cout << primo << endl;
+
+return 0;
  }
 */
 
 /*
 
-   Problema #8
+   //Problema #12 mayor divisor primo
 
 int main(){
 
+    //unsigned short int maxpri = 2; //Factor primo maximo
+    unsigned int num = 90509; //Numero para sacarle el factor primo maximo Nota: la respuesta para 1 es vacio, favor no utilizar este numero
+
+    for(unsigned int a = num/2; a > 0; a--){
+        if(esPrimo(a)==1 && num%a==0){
+            cout << "El mayor factor primo de " << num << " es: " << a << endl;
+            break;
+        }
+    }
+return 0;
  }
 */
 
 /*
 
-   Problema #10
+   //Problema #14 Numero palindromo maximo con dos numeros de tres digitos
 
 int main(){
+    unsigned int maxPali = 0; //Numero palindromo maximo encontrado
+    unsigned int maxA = 0; //Primer factor del numero palindromo
+    unsigned int maxB = 0; //Segundo factor del numero palindromo
 
+    for(int a = 100; a<=999; a++){
+        for( int b = 100; b<=999; b++){
+            if(esPali(a*b)){
+                if(maxPali < (a*b)){
+                    maxPali = a*b;
+                    maxA = a;
+                    maxB = b;
+                }
+            }
+        }
+    }
+
+    cout << maxA << "*" << maxB << "=" << maxPali << endl;
+return 0;
  }
 */
 
 /*
 
-   Problema #12
+   //Problema #16
 
 int main(){
 
+return 0;
  }
 */
 
 /*
 
-   Problema #14
+   //Problema #17
 
 int main(){
 
+return 0;
  }
 */
 
-/*
+int factorial(unsigned short int a){ //Returna el factorial de un numero
+    unsigned int fact = 1;
+    for(unsigned short int b = 1; b <= a; b++ ){
+        fact = fact*b;
+    }
+    return fact;
+}
 
-   Problema #16
+bool esPrimo(unsigned int a){ //Retorna verdadero si el numero es primo y falso si no lo es
+    bool buleano = 1;
+    for(unsigned int b = 2; b<= a/2; b++){
+        if(a%b == 0){
+            buleano = 0;
+            break;
+        }
+    }
 
-int main(){
+    return buleano;
+}
 
- }
-*/
+bool esPali(unsigned int a){ //Returna true si un numero es palindromo y false si no lo es
+    if(a == flipNum(a)){
+        return true;
+    }else{
+        return false;
+    }
 
-/*
+}
 
-   Problema #17
+int digitos(unsigned int a){
+    unsigned short int b = 0;
+    while(a>0){
+        a = a/10;
+        b += 1;
+    }
+    return b;
+}
 
-int main(){
+int flipNum(unsigned int a){
+    unsigned int flip = 0;
+    unsigned short int digits = digitos(a);
 
- }
-*/
+    for(unsigned short int b = digits; b>0 ; b--){
+        flip += (a%10)*pow(10, b-1);
+        a = a/10;
+    }
+
+    return flip;
+}
+
+//cout << "Feliz navidad y prospero " << convertir_a_espanol("year") << " nuevo profesor." << endl;
